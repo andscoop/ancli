928d0b0e55c3b53e6d11d54ca7d3df86ee7ec68a
andscoop <andycooper.s@gmail.com>
2020-08-08 11:28:01 -0700

    Create json index of cards

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -4,10 +4,27 @@ import (
 	"fmt"
 	"github.com/karrick/godirwalk"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
+type card struct {
+	fp           string
+	lastIndexed  time.Time
+	firstIndexed time.Time
+}
+
+type index struct {
+	lastIndexed time.Time
+	cards       []card
+}
+
 func isHidden(osPathname string) bool {
-	if string(osPathname[0]) == "." {
+	fileNodes := strings.Split(osPathname, "/")
+	node := fileNodes[len(fileNodes)-1]
+
+	if string(node[0]) == "." {
+		fmt.Println("true")
 		return true
 	}
 
@@ -15,11 +32,17 @@ func isHidden(osPathname string) bool {
 }
 
 func Walk(dirname string, showHidden bool) {
+	i := index{lastIndexed: time.Now(), cards: make([]card, 1)}
 	err := godirwalk.Walk(dirname, &godirwalk.Options{
 		Callback: func(osPathname string, de *godirwalk.Dirent) error {
 			if !showHidden && isHidden(osPathname) {
 				return filepath.SkipDir
 			}
+
+			// time cutoff logic is incorrect
+			c := card{fp: osPathname, lastIndexed: time.Now()}
+			i.cards = append(i.cards, c)
+
 			fmt.Printf("%s %s\n", de.ModeType(), osPathname)
 			return nil
 		},
@@ -28,4 +51,6 @@ func Walk(dirname string, showHidden bool) {
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("%+v\n", i)
 }
